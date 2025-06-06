a4b40ff0d3ea4845cc6f16969fd80fb3a7c8acb2
4×25 <1198079622@qq.com>
2025-06-06 09:07:39 +0000

    update

--- api/get2.js
+++ api/get2.js
@@ -1,3 +1,37 @@
-export function GET(request) {
-  return new Response("Hello from Vercel!");
+import axios from "axios";
+import { HttpProxyAgent } from "http-proxy-agent";
+import { HttpsProxyAgent } from "https-proxy-agent";
+
+export async function GET(request) {
+  try {
+    const proxy = await fetch(
+      "http://api.dmdaili.com/dmgetip.asp?apikey=6a0bf61f&pwd=400e52b5aef21b2c9cb728f99705803c&getnum=1&httptype=0&geshi=1&fenge=1&fengefu=&Contenttype=1&operate=all&setcity=all&provin=zhejiang"
+    ).then((res) => res.text());
+    if (typeof proxy !== "string" || !/\d+\.\d+.\d+.\d+\:\d+/.test(proxy)) {
+      throw new Error("No usable proxy.");
+    }
+    console.log("using proxy: ", proxy);
+    const httpProxyAgent = new HttpProxyAgent(`http://${proxy}`, {
+      rejectUnauthorized: false,
+    });
+    const httpsProxyAgent = new HttpsProxyAgent(`http://${proxy}`, {
+      rejectUnauthorized: false,
+    });
+
+    const options = {
+      method: "GET",
+      httpAgent: httpProxyAgent,
+      httpsAgent: httpsProxyAgent,
+    };
+    const axiosResponse = await axios("https://api.myip.la/", options);
+    console.log(typeof axiosResponse.data, axiosResponse.data);
+    return new Response(
+      JSON.stringify({
+        proxy,
+        ip: axiosResponse.data,
+      })
+    );
+  } catch (e) {
+    return new Response("Error" + e.stack);
+  }
 }
